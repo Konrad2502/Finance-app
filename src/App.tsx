dfafd014b82d27b2464b99c33240e000ffff1d7f
Konrad Litak <konrad.litak@gmail.com>
2025-12-17 20:07:00 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Container from "./components/Container";
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <>
+      <Container>hello</Container>
+    </>
+  );
+}
+
+export default App;
